2122825acba467ed74c2ecfa8a64eea03a6219ba
taes1g09 <taes1g09@ecs.soton.ac.uk>
2012-11-15 18:22:42 +0000

    Scrape of existing website. Beginning of new website including Bootstrap.js.

---
+++ old/www.mccormicksmith.co.uk/McCormick_and_Smith/CONTACT_files/CONTACT.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20121114
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,39),url:'CONTACT_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'CONTACT_files/stroke_1.png'},{rect:new IWRect(1,-1,243,2),url:'CONTACT_files/stroke_2.png'},{rect:new IWRect(244,-1,2,2),url:'CONTACT_files/stroke_3.png'},{rect:new IWRect(244,1,2,39),url:'CONTACT_files/stroke_4.png'},{rect:new IWRect(244,40,2,2),url:'CONTACT_files/stroke_5.png'},{rect:new IWRect(1,40,243,2),url:'CONTACT_files/stroke_6.png'},{rect:new IWRect(-1,40,2,2),url:'CONTACT_files/stroke_7.png'}],new IWSize(245,41)),stroke_1:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('CONTACT_files/CONTACTMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
